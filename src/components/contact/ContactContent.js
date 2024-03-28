05784a2b275a1d63cc64f3ebb460bc90b1f2f211
Ajay-Goswami <goswamiajay2002@gmail.com>
2024-03-28 22:23:16 +0530

    update

--- src/components/contact/ContactContent.js
+++ src/components/contact/ContactContent.js
@@ -27,47 +27,44 @@ function ContactContent() {
               </div>
             </div>
           </div>
-          <div className="row g-5 d-flex" style={{ alignItems: "center" }}>
-            <div class="col-lg-6 p-5">
-              <div class="">
-                <div class="d-flex flex-column bg-white p-5 rounded-lg shadow">
-                  <h2 class=" text-xxl font-weight-bold text-gray-800 mb-4">
-                    Contact Information
-                  </h2>
-                  <p class="text-gray-600 mb-4 text-start">
-                    Feel free to reach out to us using the following contact
-                    details:
-                  </p>
-                  <ul class=" pl-3 text-start">
-                    <li class="mb-2">
-                      Email:{" "}
-                      <a href="mailto:narayansewatrust2002@gmail.com">
-                        narayansewatrust2002@gmail.com
-                      </a>
-                    </li>
-                    <li class="mb-2">
-                      Phone: <a href="tel:+91-9718905221">+91-9718905221</a>
-                    </li>
-                    <li class="mb-2">
-                      Address: Flat No. T-1, 104, 1st Floor, Tower-1 No. GH
-                      01/A, Sector 16C, Exotica Dreamville, Greater Noida, U.P.
-                      201203
-                    </li>
-                  </ul>
-                </div>
-              </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <div className="bg-white py-5 rounded-lg shadow">
+              <h2 className="text-2xl font-bold text-gray-800 mb-4">
+                Contact Information
+              </h2>
+              <p className="text-gray-600 mb-4">
+                Feel free to reach out to us using the following contact
+                details:
+              </p>
+              <ul className="pl-3">
+                <li className="mb-2 pl-3">
+                  <span className="font-bold">Email:</span>{" "}
+                  <a
+                    href="mailto:narayansewatrust2002@gmail.com"
+                    className="break-all"
+                  >
+                    narayansewatrust2002@gmail.com
+                  </a>
+                </li>
+                <li className="mb-2">
+                  <span className="font-bold">Phone:</span>{" "}
+                  <a href="tel:+91-9718905221">+91-9718905221</a>
+                </li>
+                <li className="mb-2">
+                  <span className="font-bold">Address:</span> Flat No. T-1, 104,
+                  1st Floor, Tower-1 No. GH 01/A, Sector 16C, Exotica
+                  Dreamville, Greater Noida, U.P. 201203
+                </li>
+              </ul>
             </div>
-            <div className="col-lg-6 wow fadeIn mh-450" data-wow-delay="0.5s">
-              <div className="position-relative rounded overflow-hidden h-100">
-                <Iframe
-                  className="position-relative w-100 h-100 mh-450"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7158.916880268198!2d77.57079517416102!3d28.479206969946763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce56d7c91dbdb%3A0x78446b3d26e8a7d7!2sExotica%20Dreamville!5e0!3m2!1sen!2sin!4v1646109282021!5m2!1sen!2sin"
-                  frameborder="0"
-                  allowfullscreen=""
-                  aria-hidden="false"
-                  tabindex="0"
-                ></Iframe>
-              </div>
+            <div className="relative overflow-hidden rounded-lg aspect-ratio-16/9">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7158.916880268198!2d77.57079517416102!3d28.479206969946763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce56d7c91dbdb%3A0x78446b3d26e8a7d7!2sExotica%20Dreamville!5e0!3m2!1sen!2sin!4v1646109282021!5m2!1sen!2sin"
+                className="absolute inset-0 w-full h-full"
+                frameBorder="0"
+                allowFullScreen=""
+                title="Google Map"
+              ></iframe>
             </div>
           </div>
 
